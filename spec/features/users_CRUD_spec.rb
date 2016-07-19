3e83422dad51cc2368071b879d558935225579c9
Hector Huertas <hectorhuertas@gmail.com>
2016-07-20 00:27:05 +0200

    Enhance user crud testing

--- spec/features/users_CRUD_spec.rb
+++ spec/features/users_CRUD_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'user_creation', type: :feature do
+RSpec.describe 'user_CRUD', type: :feature do
   context 'New user' do
     context 'with correct data' do
       it "register successfully" do
@@ -16,5 +16,32 @@ RSpec.describe 'user_creation', type: :feature do
         expect(page).to have_text(user.username)
       end
     end
+  end
+end
+
+feature 'Visitor signs up' do
+  scenario 'with valid email and password' do
+    sign_up_with 'valid@example.com', 'password'
+
+    expect(page).to have_content('Sign out')
+  end
+
+  scenario 'with invalid email' do
+    sign_up_with 'invalid_email', 'password'
+
+    expect(page).to have_content('Sign in')
+  end
+
+  scenario 'with blank password' do
+    sign_up_with 'valid@example.com', ''
+
+    expect(page).to have_content('Sign in')
+  end
+
+  def sign_up_with(email, password)
+    visit sign_up_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Sign up'
   end
 end
